da1995d966f4e2eb11439db5b426c24f5dcb28a3
Деев Алексей <adeev@neoflex.ru>
2021-05-26 09:53:00 +0300

    Initial commit

---
+++ src/test/kotlin/ru/alxdv/reactiveproject/ReactiveProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.alxdv.reactiveproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
